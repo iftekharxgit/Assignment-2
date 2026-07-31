a5a05fc73c03725289d5c6c148d399fcb85d7131
iftekharxgit <iftekharasifx96@gmail.com>
2026-07-31 23:20:39 +0600

    Assignment-2 almost complete

--- problem-4.js
+++ problem-4.js
@@ -12,9 +12,3 @@ function topRatedRestaurant(restaurants) {
    }
    return topRestaurant.name.toUpperCase();
 }
-// console.log(topRatedRestaurant([
-//     { name: "Chillox", rating: 4.5 },
-//     { name: "Sultan's Dine", rating: 4.8 }
-// ]));
-
-// console.log(topRatedRestaurant([]));
